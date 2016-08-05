a6bb128bf6048eca2a876821930358c3e07506b5
Dalton Petursson <dalton@driveshift.com>
2016-08-05 11:33:17 -0700

    adds init script that copies dotfiles to home dir and sources them

---
+++ init_dotfiles.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+curdir=${PWD##*/}
+
+if [ $curdir != ".dotfiles" ]
+	then
+		echo "This script should be run from inside the .dotfiles directory. Exiting."
+		exit
+fi
+
+file=".vimrc"
+echo "Overwriting ~/$file"
+cp $file ~/$file
+
+file=".bash_profile"
+echo "Overwriting ~/$file"
+cp $file ~/$file
+
+echo "Sourcing ~/$file"
+source ~/$file
+
+echo ".dotfiles init complete!"
+exit
