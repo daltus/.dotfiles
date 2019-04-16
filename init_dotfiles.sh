a16492601953f7d0ecc2faac1532a32e516ed470
daltus <dpetursson@gmail.com>
2019-04-15 20:09:09 -0700

    added version to the git scripts that get curl'd
    due to bug described in:
    https://apple.stackexchange.com/questions/327817/git-completion-bash-producing-error-on-macos-sierra-10-12-6

--- init_dotfiles.sh
+++ init_dotfiles.sh
@@ -8,13 +8,16 @@ if [ $curdir != ".dotfiles" ]
 		exit
 fi
 
+# Use files for git 2.17.1
+gitver="v2.17.1"
+
 file="git-completion.bash"
 echo "curl'ing $file from master branch in git's github repo."
-curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/$file" -o $file
+curl "https://raw.githubusercontent.com/git/git/$gitver/contrib/completion/$file" -o $file
 
 file="git-prompt.sh"
 echo "curl'ing $file from master branch in git's github repo."
-curl "https://raw.githubusercontent.com/git/git/master/contrib/completion/$file" -o $file
+curl "https://raw.githubusercontent.com/git/git/$gitver/contrib/completion/$file" -o $file
 
 file=".vimrc"
 echo "Overwriting ~/$file"
